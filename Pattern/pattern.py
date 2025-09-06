9d0385f552a5902689919e408dc38b43102a2f25
SRUAJN R <codewithsrujan.r@gmail.com>
2025-09-07 00:30:23 +0530

    Update pattern.py

--- Pattern/pattern.py
+++ Pattern/pattern.py
@@ -1,128 +1,30 @@
-def print1(n):
-    for i in range(n, 0,-1):
-        print("*"*i)
-def print2(n):
-    for i in range(1,n+1):
-        for j in range(1,n-i+1+1):
-            print(j,end="")
-        print()
-def print3(n):
-    for i in range(0,n):
-        for j in range(0,n-i-1):
-            print(" ",end="")
-        for k in range(i*2+1):
-            print("*",end="")
-        for l in range(0,n-i-1):
-            print(" ",end="")
-        print()
-def print4(n):
-    for i in range(0, n):
-        for j in range(0,i):
-            print(" ",end="")
-        for k in range((n*2)-1-(2*i)):
-            print("*",end="")
-        for l in range(0,i):
-            print(" ",end="")
-        print()
-def print5(n):
-    for i in range(0,n):
-        for j in range(0,n-i-1):
-            print(" ",end="")
-        for k in range(i*2+1):
-            print("*",end="")
-        for l in range(0,n-i-1):
-            print(" ",end="")
-        print()
-    for i in range(0, n):
-        for j in range(0,i):
-            print(" ",end="")
-        for k in range((n*2)-1-(2*i)):
-            print("*",end="")
-        for l in range(0,i):
-            print(" ",end="")
-        print()
-def print6(n):
-    for i in range(1,n+1):
-        print("*"*i)
-    for i in range(n-1,0,-1):
-        print("*" * i)
+def print1(n): """Decreasing triangle of stars""" for i in range(n, 0, -1): print("*" * i) # Example for n=4: # **** # *** # ** # *
 
-def print7(n):
-    for i in range(1,n+1):
-        for j in range(1,i+1):
-            if((i+j)%2==0):
-                print("1",end="")
-            if((i+j)%2!=0):
-                print("0", end="")
-        print()
-        """hard"""
-def print8(n):
-    for i in range(1,n+1):
-        for j in range(1,i+1):
-            print(j,end="")
-        for k in range((n-1)*2):
-            print("-",end="")
-        n=n-1
-        for l in range(i,0,-1):
-            print(l, end="")
-        print()
+def print2(n): """Numbers decreasing in length""" for i in range(1, n + 1): for j in range(1, n - i + 2): print(j, end="") print() # Example for n=4: # 1234 # 123 # 12 # 1
 
-def print9(n):
-    num=1
-    for i in range(1,n+1):
-        for j in range(1,i+1):
-            print(f" {num}",end="")
-            num=num+1
-        print()
+def print3(n): """Centered pyramid of stars""" for i in range(n): print(" " * (n - i - 1) + "*" * (2 * i + 1)) # Example for n=4: #    * #   *** #  ***** # *******
 
-def print10(n):
-    for i in range(1,n+1):
-        for j in range(n):
-            print(chr(65+j),end="")
-        n=n-1
-        print()
-def print11(n):
-    for i in range(1,n+1):
-        for j in range(i):
-            print(chr(65+j),end="")
-        print()
+def print4(n): """Inverted centered pyramid of stars""" for i in range(n): print(" " * i + "*" * (2 * (n - i) - 1)) # Example for n=4: # ******* #  ***** #   *** #    *
 
-def print12(n):
-    for i in range(0, n + 1):
-        for j in range(i):
-            print(chr(64+i),end="")
-        print()
-def print18(n):
-    for i in range(n):
-        for j in range(i+1):
-            print(chr(69-j),end="")
-        print()
-def print19(n):
-    for i in range(n//2):
-        stars = n//2 - i
-        dashes = i * 2
-        print("*"*stars + "-"*dashes + "*"*stars)
-    for i in range(n//2, 0, -1):
-        stars = n//2 - i
-        dashes = i * 2
-        print("*"*stars + "-"*dashes + "*"*stars)
+def print5(n): """Diamond of stars""" for i in range(n): print(" " * (n - i - 1) + "" * (2 * i + 1)) for i in range(n): print(" " * i + "" * (2 * (n - i) - 1)) # Example for n=3: #   * #  *** # ***** # ***** #  *** #   *
 
+def print6(n): """Triangle up and down""" for i in range(1, n + 1): print("" * i) for i in range(n - 1, 0, -1): print("" * i) # Example for n=3: # * # ** # *** # ** # *
 
+def print7(n): """0-1 triangle pattern""" for i in range(1, n + 1): for j in range(1, i + 1): print("1" if (i + j) % 2 == 0 else "0", end="") print() # Example for n=4: # 1 # 01 # 101 # 0101
 
-def print20(n):
-    for i in range(n):
-        if(i==0 or i==n-1):
-            print("*"*n)
-        else:
-            print("*"+" "*(n-2)+"*")
+def print8(n): """Number palindrome with dashes in center""" for i in range(1, n + 1): print("".join(str(j) for j in range(1, i + 1)), end="") print("-" * ((n - i) * 2), end="") print("".join(str(j) for j in range(i, 0, -1))) # Example for n=4: # 1------1 # 12----21 # 123--321 # 12344321
 
+def print9(n): """Continuous numbers in triangle""" num = 1 for i in range(1, n + 1): for _ in range(i): print(f" {num}", end="") num += 1 print() # Example for n=4: #  1 #  2 3 #  4 5 6 #  7 8 9 10
 
+def print10(n): """Letters decreasing in rows""" for i in range(n, 0, -1): print("".join(chr(65 + j) for j in range(i))) # Example for n=4: # ABCD # ABC # AB # A
 
+def print11(n): """Letters increasing in rows""" for i in range(1, n + 1): print("".join(chr(65 + j) for j in range(i))) # Example for n=4: # A # AB # ABC # ABCD
 
+def print12(n): """Same letter repeated in row""" for i in range(1, n + 1): print(chr(64 + i) * i) # Example for n=4: # A # BB # CCC # DDDD
 
+def print18(n): """Reverse letters triangle""" for i in range(n): print("".join(chr(69 - j) for j in range(i + 1))) # Example for n=4: # E # ED # EDC # EDCB
 
+def print19(n): """Hourglass with stars and dashes""" for i in range(n // 2): stars = n // 2 - i dashes = i * 2 print("" * stars + "-" * dashes + "" * stars) for i in range(n // 2, 0, -1): stars = n // 2 - i dashes = i * 2 print("" * stars + "-" * dashes + "" * stars) # Example for n=6: # -- # ---- # ---- # --
 
+def print20(n): """Hollow square""" for i in range(n): if i == 0 or i == n - 1: print("" * n) else: print("" + " " * (n - 2) + "*") # Example for n=4: # **** # *  * # *  * # ****
 
-
-
-print20(4)
